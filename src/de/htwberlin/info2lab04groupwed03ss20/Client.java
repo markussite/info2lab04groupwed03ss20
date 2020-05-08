ba47607bf5587dd886cbf0da76dfc4829c75665a
Marcel Kuehnau <s0573132@htw-berlin.de>
2020-05-08 17:01:23 +0200

    janiks magic working code

--- src/de/htwberlin/info2lab04groupwed03ss20/Client.java
+++ src/de/htwberlin/info2lab04groupwed03ss20/Client.java
@@ -16,13 +16,20 @@ public class Client {
         try (
                 Socket echoSocket = new Socket(hostName, portNumber);
                 PrintWriter out = new PrintWriter(echoSocket.getOutputStream(), true);
+
                 BufferedReader in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
                 BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in))
         ) {
             String userInput;
             while (!(userInput = stdIn.readLine()).equals("quit")) {
                 out.println(userInput);
-                System.out.println("echo: " + in.readLine());
+                System.out.println("input: " + in.readLine());
+
+                String answer;
+                answer = in.readLine();
+                System.out.println("Server: " + answer);
+
+
             }
         } catch (UnknownHostException e) {
             System.err.println("Don't know about host " + hostName);
--- src/de/htwberlin/info2lab04groupwed03ss20/Server.java
+++ src/de/htwberlin/info2lab04groupwed03ss20/Server.java
@@ -15,17 +15,26 @@ public class Server {
                     ServerSocket serverSocket = new ServerSocket(Integer.parseInt(args[0]));
                     Socket clientSocket = serverSocket.accept();
                     PrintWriter outding = new PrintWriter(clientSocket.getOutputStream(), true);
-                    BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
-                    
+                    BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+                    BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in))
             ) {
                 String inputLine;
+
                 while ((inputLine = in.readLine()) != null) {
+
+
+                    //echo ding
                     outding.println(inputLine);
-                    System.out.println(inputLine);
+
+                    System.out.println("client: "+inputLine);
                     if(inputLine.equals("quit")){
                         clientSocket.close();
                         serverSocket.close();
                     }
+                    String ding = stdIn.readLine();
+                    System.out.println("input: " + ding);
+                    outding.println(ding);
                 }
             } catch (IOException e) {
                 System.out.println("Exception caught when trying to listen on port "
